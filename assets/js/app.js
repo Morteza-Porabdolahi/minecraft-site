6d0b801abba15d428b0c6e15f4394420f4ff243a
Michel Jackson <mortazapor3@gmail.com>
2022-01-27 16:59:41 +0330

    master

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
+// -------- change text -------------//
+let texts = ['D', 'De', 'Del', 'Delt', 'Delta', 'Delta M', 'Delta Mi', 'Delta Min', 'Delta Mine', 'Delta Minec', 'Delta Minecr', 'Delta Minecra', 'Delta Minecraf', 'Delta Minecraft', 'Delta Minecraft S', 'Delta Minecraft Se', 'Delta Minecraft Ser', 'Delta Minecraft Serv', 'Delta Minecraft Serve', 'Delta Minecraft Server', 'Delta Minecraft Serve', 'Delta Minecraft Serv', 'Delta Minecraft Ser', 'Delta Minecraft Se', 'Delta Minecraft S', 'Delta Minecraft', 'Delta Minecraf', 'Delta Minecra', 'Delta Minecr', 'Delta Minec', 'Delta Mine', 'Delta Min', 'Delta Mi', 'Delta M', 'Delta', 'Delt', 'Del', 'De', 'D'];
+let number = 0;
+let html = document.getElementById('txt-rotate');
+function txtRotate() {
+  number++;
+  if (number >= texts.length) {
+    number = 0;
+  }
+  html.innerHTML = texts[number];
+}
+setInterval(txtRotate, 300);
+// ----------------- menu ---------------- //
 $('#burger-menu').click(function (e) {
   $('.header-list').toggleClass('flex');
-  if($('.header-list').hasClass('flex')){
+  if ($('.header-list').hasClass('flex')) {
     $('.burger').addClass('fa-times');
-  }else{
+  } else {
     $('.burger').addClass('fa-bars');
   }
 });
+// -------- Slider ------------- //
 var index = 0;
 showSlides();
 function showSlides() {
@@ -32,59 +46,19 @@ function showSlides() {
   setTimeout(showSlides, 7000);
 }
 
-var TxtRotate = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+let searchBtn = document.getElementById('search-btn');
+let searchInput = document.getElementById('search-input');
+let lists = document.getElementsByClassName('history-list-item');
+let x = 0;
+x--;
+searchBtn.addEventListener('click', function () {
+  if(!isNaN(searchInput.value)){
+    return;
   }
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
+  x++;
+  if (x >= lists.length) {
+    x = 0;
+    lists[x].innerHTML = '';
   }
-
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function () {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+  lists[x].innerHTML = searchInput.value;
+});
