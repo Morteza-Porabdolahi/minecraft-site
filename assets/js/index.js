079599801dd045d0a94d615fac0ca33686353369
Michel Jackson <mortazapor3@gmail.com>
2022-02-03 14:46:17 +0330

    master

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,4 +60,20 @@ window.onscroll = function styleButton () {
 backToTopButton.addEventListener('click',function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-})
+});
+$('#account-buy-btn').click(function (e) { 
+    $('body').css({'overflow' : 'hidden'});
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $('#account-buy-modal').css({
+        'display' : 'flex',
+        'opacity' : 0,
+    }).animate({'opacity' : 1},200);
+});
+$('.hide-modal').click(function (e) { 
+    $('body').css({'overflow' : 'auto'});
+    $('#account-buy-modal').css({
+        'display' : 'none',
+        'opacity' : 1,
+    }).animate({'opacity' : 0},400);
+});
